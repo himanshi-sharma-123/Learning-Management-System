fc9479ea31013da902524b8647ac6e554fb24df3
Himanshi Sharma <himanshish456@gmail.com>
2024-01-23 12:03:44 +0530

    authentication

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
